859f35058ee25b40209e8097cc10c715e33952c0
DevilTanuki99 <wibuakun2596@gmail.com>
2025-11-25 14:40:43 +0700

    Update project

---
+++ src/script/script-blog.js
@@ -0,0 +1,42 @@
+const buttons = document.querySelectorAll(".category-btn");
+const cards = document.querySelectorAll(".project-item");
+const searchBox = document.getElementById("searchBox");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+    // 1. Highlight tombol aktif
+    buttons.forEach(btn => {
+      btn.classList.remove("bg-cyan-500", "text-black");
+      btn.classList.add("bg-gray-800", "text-cyan-300");
+    });
+
+    button.classList.add("bg-cyan-500", "text-black");
+    button.classList.remove("bg-gray-800", "text-cyan-300");
+
+    // 2. Filter project sesuai category
+    const category = button.getAttribute("data-category");
+    cards.forEach(card => {
+      if (category === "all" || card.classList.contains(category)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+
+    // 3. Reset search box
+    searchBox.value = '';
+  });
+});
+
+// 4. Search filter
+searchBox.addEventListener("input", function() {
+  let query = this.value.toLowerCase();
+  cards.forEach(card => {
+    card.style.display = card.textContent.toLowerCase().includes(query) ? "block" : "none";
+  });
+  // Hapus highlight tombol ketika search
+  buttons.forEach(btn => {
+    btn.classList.remove("bg-cyan-500", "text-black");
+    btn.classList.add("bg-gray-800", "text-cyan-300");
+  });
+});
